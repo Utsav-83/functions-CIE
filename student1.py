5f33afacdd3e72df6ed68b82000a80e03e26e759
Utsav-83 <utsav8391@gmail.com>
2025-12-02 12:32:16 +0530

    Update student1.py

--- student1.py
+++ student1.py
@@ -1,4 +1,3 @@
-
 def Student_details(Student_ID, Student_Name, Course_Enrolled, Academic_Year):
     result = (
         f"Student ID: {Student_ID}\n"
@@ -8,12 +7,6 @@ def Student_details(Student_ID, Student_Name, Course_Enrolled, Academic_Year):
     )
     return result
 
-if __name__ == "__main__":
-    Student_ID = 105
-    Student_Name = "Naman"
-    Course_Enrolled = "BCA"
-    Academic_Year = "2026"
-    
-    print(student_details(Student_ID, Student_Name, Course_Enrolled, Academic_Year))
 
-    
+if __name__ == "__main__":
+    print(Student_details(105, "Naman", "BCA", "2026"))
